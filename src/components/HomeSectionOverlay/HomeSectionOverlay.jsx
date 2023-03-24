4951b9fe9ce3e8a0fa0935ac90ed7194891ae269
Milos Micevic <milos.micevic@asioso.com>
2023-03-25 00:08:26 +0100

    Adjusting Hero

--- src/components/HomeSectionOverlay/HomeSectionOverlay.jsx
+++ src/components/HomeSectionOverlay/HomeSectionOverlay.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomeSectionOverlay.scss";
 
 const HomeSectionOverlay = () => {
-  return <div className="absolute top-0 left-0 w-full h-full home-section-overlay opacity-40"></div>;
+  return <div className="absolute top-0 left-0 w-full h-full min-h-screen home-section-overlay opacity-40"></div>;
 };
 
 export default HomeSectionOverlay;
